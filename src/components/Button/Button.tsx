3858eb1769707ea96895ce69ca9d9c291b924fda
linfeng <linfeng.tian@ximalaya.com>
2019-03-21 17:16:32 +0800

    ts init

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,24 +1,50 @@
 import * as React from 'react';
 import { Component } from 'react';
+// import * as PropTypes from 'prop-types';
+import classNames from 'classnames';
+import './Button.scss'
 
-/**
- * Column properties.
- */
-export interface IColumnProps {
-  /** prop1 description */
-  prop1?: string;
-  /** prop2 description */
-  prop2: number;
-  /** prop4 description */
-  prop3: 'option1' | 'option2' | 'option3';
+export interface ButtonProps {
+  /** 前缀 */
+  prefix?: string;
+  /** 类名 */
+  className?: string;
+  /** 主题 */
+  theme?: 'primary';
+  /** 是否禁用 */
+  disabled?: boolean;
+  /** 点击按钮时的回调 */
+  onClick?: () => void;
+  /** 尺寸 */
+  size?: 'sm' | 'md' | 'lg';
+  /** 设置按钮的图标类型 */
+  icon?: string;
+  /** 子元素 */
+  children?: React.ReactNode;
 }
 
 /**
  * 按钮
  */
-class Button extends Component<IColumnProps, {}> {
+class Button extends Component<ButtonProps, any> {
+  static defaultProps = {
+    prefix: 'hmly',
+    theme: 'primary',
+    size: 'md',
+    disabled: false
+  }
+
   render() {
-    return <div>Button</div>;
+    const { prefix, className, theme, size, disabled, ...others } = this.props
+    const classes = classNames(`${prefix}-button`, {
+      [`${prefix}-button-${theme}`]: theme,
+      [`${prefix}-button-${size}`]: size,
+      [`${prefix}-button-disabled`]: disabled
+    }, className)
+
+    return (<div className={classes}>
+      Button
+    </div>);
   }
 }
 
