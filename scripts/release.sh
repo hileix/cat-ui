824974b6d55ea2ea6b58052f4a242602ba1a5a26
linfeng <linfeng.tian@ximalaya.com>
2019-03-27 15:53:46 +0800

    script init

---
+++ scripts/release.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+upgradeVersion() {
+  versionLine=$(grep version package.json)
+  versionNum=$(echo ${versionLine} | tr -cd "[0-9].[0-9]")
+  subVersionNum=$(echo ${versionNum##*.})
+  newSubVersionNum=`expr $subVersionNum + 1`
+  newVersionNum=$(echo ${versionNum/${subVersionNum}/${newSubVersionNum}})
+  newVersionLine=$(echo "${versionLine/${versionNum}/${newVersionNum}}")
+  sed -i "" "s/${versionLine}/${newVersionLine}/g" "package.json"
+  echo "package.json更新成功：${newVersionLine}"
+}
+
+upgradeVersion
+
+npm publish --registry http://xnpm.ximalaya.com
+
+if [ $? -eq 0 ]
+then
+  git commit -am "release:script"
+  git push
+  git status
+  echo "发布成功: ${newVersionLine}"
+else
+  echo "发布失败: ${newVersionLine}"
+fi
