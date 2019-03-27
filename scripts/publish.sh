44d3074b7afc9b41b1f008f8aaa6ff4b2794c0a3
linfeng <linfeng.tian@ximalaya.com>
2019-03-27 15:36:05 +0800

    publish:script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,17 +2,25 @@
 
 set -e
 
-versionLine=$(grep version package.json)
-versionNum=$(echo ${versionLine} | tr -cd "[0-9].[0-9]")
-subVersionNum=$(echo ${versionNum##*.})
-newSubVersionNum=`expr $subVersionNum + 1`
-newVersionNum=$(echo ${versionNum/${subVersionNum}/${newSubVersionNum}})
-newVersionLine=$(echo "${versionLine/${versionNum}/${newVersionNum}}")
-sed -i "" "s/${versionLine}/${newVersionLine}/g" "package.json"
-echo "package.json更新成功：${newVersionLine}"
+upgradeVersion() {
+  versionLine=$(grep version package.json)
+  versionNum=$(echo ${versionLine} | tr -cd "[0-9].[0-9]")
+  subVersionNum=$(echo ${versionNum##*.})
+  newSubVersionNum=`expr $subVersionNum + 1`
+  newVersionNum=$(echo ${versionNum/${subVersionNum}/${newSubVersionNum}})
+  newVersionLine=$(echo "${versionLine/${versionNum}/${newVersionNum}}")
+  sed -i "" "s/${versionLine}/${newVersionLine}/g" "package.json"
+  echo "package.json更新成功：${newVersionLine}"
+}
 
-git commit -am "publish:script"
-git push
-# npm publish --registry http://xnpm.ximalaya.com
+upgradeVersion
 
-echo "发布成功: ${newVersionLine}"
+if [ $? -eq 0 ]
+then
+  git commit -am "publish:script"
+  git push
+  git status
+  echo "发布成功: ${newVersionLine}"
+else
+  echo "发布失败: ${newVersionLine}"
+fi
