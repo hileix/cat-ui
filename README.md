477c43939cb68d05f364ddfec4e1ec346d4ee823
linfeng <linfeng.tian@ximalaya.com>
2019-04-02 15:43:56 +0800

    new-component

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 ## 分支简介
 
-- `master`分支是稳定分支，用以正式线上项目
+- `master`分支是稳定分支，用以正式线上环境
 - `develop`分支是开发分支，用以开发、测试、预览
 
 `master`分支的`git`提交，会自动发布`styleguidist`预览
@@ -19,6 +19,9 @@ $ yarn
 启动服务器
 $ yarn run dev
 
+新增组件，会新增一个空白组件模板，组件名首字母需大写
+$ yarn run new [组件名]
+
 发布到xnpm
 $ yarn run release 自动迭代版本号，递增最后一位
 $ yarn run release -i 忽略自动迭代，不要自动迭代版本号
@@ -27,22 +30,6 @@ $ yarn run release 1.6.2 使用用户传入的版本号
 运行单元测试
 $ yarn run test
 ```
-
-## 新增组件
-
-以`Button`为例说明
-
-- 在`Button.tsx`中的`class Button extends Component`前面添加注释，以说明该组件
-- 在`Button.tsx`中的`Button`的`interface`为各个参数添加注释，以说明该参数
-- 将`Button.md`中编写各个示例
-- 将`__tests__/index.test.tsx`中编写单元测试
-- 引入其他组件，参考`ButtonGroup.md`
-
-推荐规范
-
-- 在组件文件下尽量不要再新增文件夹，一个层级就够了
-- 有多个子组件的时候，每个子组件使用一个`markdown`文件
-- 子组件在`markdown`文件示范的时候，使用`CheckboxGroup`这种，无法使用`Checkbox.Group`
 
 ## 使用组件
 
---
+++ scripts/new-component.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+set -e
+
+input=$1
+
+mkdir src/components/${input}
+mkdir src/components/${input}/__test__
+mkdir src/components/${input}/styled
+
+touch src/components/${input}/index.ts
+touch src/components/${input}/${input}.tsx
+touch src/components/${input}/${input}.md
+touch src/components/${input}/styled/index.tsx
+touch src/components/${input}/__test__/index.test.tsx
+
+# 导入${input}/${input}.tsx默认模块
+echo "import * as React from 'react'
+import { Component } from 'react'
+
+export interface ${input}Props {
+  /** 类名 */
+  className?: string;
+  /** 样式 */
+  style?: object;
+}
+
+/**
+ * ${input}
+ */
+class ${input} extends Component<${input}Props, any> {
+
+  render() {
+    return (
+      <div>
+        ${input}
+      </div>
+    )
+  }
+}
+
+export default ${input}
+" >> src/components/${input}/${input}.tsx
+
+# 导入${input}/index.ts默认模块
+echo "import ${input} from './${input}'
+
+export default ${input}" >> src/components/${input}/index.ts
+
+# 导入${input}/${input}.md默认模块
+echo "Example
+
+\`\`\`jsx
+<${input}>${input}</${input}>
+
+<style>
+  {\`
+
+  \`}
+</style>
+\`\`\`
+" >> src/components/${input}/${input}.md
+
+# 导入${input}/styled/index.tsx默认模块
+echo "import styled from 'styled-components'
+import { primaryBtnColor } from '../../../styles/theme'
+
+const Styled${input} = styled.div\`
+  color: red;
+\`;
+
+export { Styled${input} }
+" >> src/components/${input}/styled/index.tsx
+
+# 导入${input}/__test__/index.test.tsx默认模块
+echo "import * as React from 'react'
+import * as Enzyme from 'enzyme'
+import { render } from 'enzyme'
+import * as Adapter from 'enzyme-adapter-react-16'
+import ${input} from '../index'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe('${input}', () => {
+  test('renders correctly', () => {
+    const wrapper = render(
+      <${input}>Test</${input}>
+    )
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  test('two plus two is four', () => {
+    expect(2 + 2).toBe(4)
+  })
+})
+" >> src/components/${input}/__test__/index.test.tsx
+
+echo -e "\033[32m \n新建组件成功: ${input}\n \033[0m"
