f4005def9aee2e3a4ce166b142162e58f3652b37
linfeng <linfeng.tian@ximalaya.com>
2019-04-02 18:58:52 +0800

    Tabs

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export { default as Button } from './Button'
 export { default as Modal } from './Modal'
+export { default as Tabs } from './Tabs'
 // export { default as Popup } from './Popup'
 // export { default as Tooltip } from './Tooltip'
 // export { default as Slider } from './Slider'
