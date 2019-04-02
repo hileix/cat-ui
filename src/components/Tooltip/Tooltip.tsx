99e8498da50fdf73df811e55f82fb6fa0abf2a33
linfeng <linfeng.tian@ximalaya.com>
2019-04-02 14:53:50 +0800

    export

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import classNames from 'classnames'
-import { Popup } from '../../components'
+import Popup from '../Popup'
 import './Tooltip.scss'
 
 export type TooltipPlacement =
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Button } from './Button'
-export { default as Popup } from './Popup'
-export { default as Tooltip } from './Tooltip'
-export { default as Slider } from './Slider'
+export { default as Modal } from './Modal'
+// export { default as Popup } from './Popup'
+// export { default as Tooltip } from './Tooltip'
+// export { default as Slider } from './Slider'
