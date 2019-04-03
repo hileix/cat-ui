8b75845af8dde233fbd09bff1943095bc210fc61
linfeng <linfeng.tian@ximalaya.com>
2019-04-03 14:29:17 +0800

    release:script

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -18,7 +18,7 @@ export interface TabsProps {
  * Tabs
  */
 class Tabs extends Component<TabsProps, any> {
-  static TabsPanel: typeof TabPanel;
+  static TabsPanel = TabPanel;
 
   toRender = () => {
     const { children } = this.props
