3cb59f5402a4044884e7be45bb7492794d7c5f49
hileix <304192604@qq.com>
2019-09-09 11:25:07 +0800

    ✨ (Message) 更新 Message 组件

--- src/components/Message/MessageContent.tsx
+++ src/components/Message/MessageContent.tsx
@@ -1,27 +1,42 @@
-import * as React from 'react'
-import classNames from 'classnames'
-import { Component } from 'react'
-import { StyledMessageContent } from './styled'
+import * as React from 'react';
+import classNames from 'classnames';
+import { Component } from 'react';
+import { StyledMessageContent } from './styled';
 import Portal from '../Portal'
+import Icon from '../Icon';
+
+export type MessageType = 'success' | 'warn' | 'error';
+
 
 export interface MessageContentProps {
-  /** 弹层是否可见 */
   text?: string;
-  type?: string;
+  type?: MessageType;
+}
+
+const IconTypeMap = {
+  success: Icon.Type.success,
+  warn: Icon.Type.warning,
+  error: Icon.Type.error
 }
 
 class MessageContent extends Component<MessageContentProps, any> {
-
   render() {
-    const { text, type } = this.props
-    const classes = classNames('message-content', `theme-${type}`)
-    // console.log('MessageContent:visible', visible)
+    const { text, type } = this.props;
+    const prefix = 'hmly';
+    const classes = classNames(`${prefix}-message`, {
+      [`${prefix}-message--success`]: type === 'success',
+      [`${prefix}-message--warn`]: type === 'warn',
+      [`${prefix}-message--error`]: type === 'error'
+    });
 
-    return (<Portal visible>
-      <StyledMessageContent className={classes}>
-        {text}
-      </StyledMessageContent>
-    </Portal>)
+    return (
+      <Portal visible>
+        <StyledMessageContent className={classes}>
+          <Icon type={IconTypeMap[type]} className={`${prefix}-message__icon`} />
+          <span className={`${prefix}-message__text`}>{text}</span>
+        </StyledMessageContent>
+      </Portal>
+    )
   }
 }
 
