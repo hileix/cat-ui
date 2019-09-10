5be7d3e06041e8053de8d9b6e4aef5303a1a05ba
hileix <304192604@qq.com>
2019-09-10 18:02:03 +0800

    update

--- scripts/scss2css.js
+++
@@ -1,7 +0,0 @@
-const sass = require('sass');
-const path = require('path');
-
-
-const result = sass.renderSync({ file: path.resolve(__dirname, '../src/components/Button/styled/Button.scss') });
-
-console.log({ result })
