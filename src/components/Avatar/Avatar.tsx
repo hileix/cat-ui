764c7e5c2ce35ef047af672035f8e23422eef10b
hileix <304192604@qq.com>
2019-09-11 09:58:46 +0800

    ✨ (Avatar) 使用 scss

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,7 +1,6 @@
-import * as React from 'react'
-import { Component } from 'react'
-import classNames from 'classnames'
-import { StyledAvatar } from './styled'
+import * as React from 'react';
+import { Component } from 'react';
+import classNames from 'classnames';
 
 export interface AvatarProps {
   /** 类名 */
@@ -25,41 +24,53 @@ export interface AvatarProps {
 
   prefix?: string;
 
-  onError?: (e: any)=> boolean;
+  onError?: (e: any) => boolean;
 }
 
 /**
  * Avatar
  */
 class Avatar extends Component<AvatarProps, any> {
-
-  handleImgLoadError = (e: any) =>{
-    this.props.onError(e)
-  }
+  handleImgLoadError = (e: any) => {
+    this.props.onError(e);
+  };
 
   render() {
-    const { prefix = 'hmly-avatar',shape, className, children, src, alt,srcSet, size, onError, ...restProps } = this.props
-    if(!src) return null
+    const {
+      prefix = 'hmly-avatar',
+      shape,
+      className,
+      children,
+      src,
+      alt,
+      srcSet,
+      size,
+      onError,
+      ...restProps
+    } = this.props;
+    if (!src) return null;
 
-    const sizeCls = classNames(`${prefix}`, {
+    const sizeCls = classNames({
       [`${prefix}-lg`]: size === 'large',
-      [`${prefix}-sm`]: size === 'small',
-    })
+      [`${prefix}-sm`]: size === 'small'
+    });
 
     const classes = classNames(prefix, className, sizeCls, {
       [`${prefix}-${shape}`]: shape,
-      [`${prefix}-image`]: src,
+      [`${prefix}-image`]: src
     });
-    
-    
+
     return (
-      <StyledAvatar className={classes} prefix={prefix}
-        {...restProps}
-      >
-        <img src={src} alt={alt} srcSet={srcSet} onError={this.handleImgLoadError} />
-      </StyledAvatar>
-    )
+      <span className={classes} prefix={prefix} {...restProps}>
+        <img
+          src={src}
+          alt={alt}
+          srcSet={srcSet}
+          onError={this.handleImgLoadError}
+        />
+      </span>
+    );
   }
 }
 
-export default Avatar
+export default Avatar;
