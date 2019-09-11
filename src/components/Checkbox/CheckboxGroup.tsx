87eca1d7f9c9d81b11301b85eff6f3050ba8f085
hileix <304192604@qq.com>
2019-09-11 12:19:22 +0800

    ✨ (Checkbox) 使用 scss

--- src/components/Checkbox/CheckboxGroup.tsx
+++ src/components/Checkbox/CheckboxGroup.tsx
@@ -1,7 +1,6 @@
-import * as React from 'react'
-import { Component, cloneElement } from 'react'
-import classNames from 'classnames'
-import { StyledCheckboxGroup } from './styled'
+import * as React from 'react';
+import { Component, cloneElement } from 'react';
+import classNames from 'classnames';
 
 export interface CheckboxGroupProps {
   /** 类名 */
@@ -26,69 +25,83 @@ export interface CheckboxGroupProps {
  * CheckboxGroup
  */
 class CheckboxGroup extends Component<CheckboxGroupProps, any> {
-  constructor (props: CheckboxGroupProps) {
-    super(props)
-    let value = []
+  constructor(props: CheckboxGroupProps) {
+    super(props);
+    let value = [];
     if ('value' in props) {
-      value = props.value
+      value = props.value;
     } else if ('defaultValue' in props) {
-      value = props.defaultValue
+      value = props.defaultValue;
     }
     this.state = {
       value: value
-    }
+    };
   }
 
-  static getDerivedStateFromProps (nextProps: CheckboxGroupProps) {
+  static getDerivedStateFromProps(nextProps: CheckboxGroupProps) {
     if ('value' in nextProps) {
+      console.log('nextValue:', nextProps.value);
       return {
         value: nextProps.value
-      }
-    } else { return null }
+      };
+    } else {
+      return null;
+    }
   }
 
   onCheckboxChange = (e: any) => {
-    const { value } = this.state
-    const { onChange } = this.props
-    const newValue = e.target.value
-    const optionIndex = value.indexOf(newValue)
+    const { value } = this.state;
+    const { onChange } = this.props;
+    const newValue = e.target.value;
+    const optionIndex = value.indexOf(newValue);
+
+    const values = [...value];
     if (optionIndex === -1) {
-      value.push(newValue)
+      values.push(newValue);
     } else {
-      value.splice(optionIndex, 1)
+      values.splice(optionIndex, 1);
     }
     // 是否有value值传递下来
     if (!('value' in this.props)) {
-      this.setState({ value })
+      this.setState({ value: values });
     }
-    onChange && onChange(value)
-  }
+    onChange && onChange(values);
+  };
 
   render() {
-    const self = this
-    const { value } = this.state
-    const { className, style, disabled, readOnly, checked, children } = this.props
-    const classes = classNames('hmly-Checkbox-group', className)
+    const { value } = this.state;
+    const {
+      className,
+      style,
+      disabled,
+      readOnly,
+      checked,
+      children
+    } = this.props;
+    const classes = classNames('hmly-checkbox-group', className);
     const Checkboxs = React.Children.map(children, (element: any, index) => {
-      if (!element) { return element }
+      if (!element) {
+        return element;
+      }
+
+      const checked = value.indexOf(element.props.value) > -1;
+      console.log({ value, checked });
 
       return cloneElement(element, {
-        onChange: self.onCheckboxChange,
+        onChange: this.onCheckboxChange,
         key: value,
-        checked: value.indexOf(element.props.value) > -1,
+        checked: checked,
         disabled: disabled,
         readOnly: readOnly
-      })
-    })
+      });
+    });
 
     return (
-      <StyledCheckboxGroup
-        className={classes}
-        style={style}>
+      <div className={classes} style={style}>
         {Checkboxs}
-      </StyledCheckboxGroup>
-    )
+      </div>
+    );
   }
 }
 
-export default CheckboxGroup
+export default CheckboxGroup;
