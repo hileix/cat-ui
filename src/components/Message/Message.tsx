8f4d516b02ccafd187ff1004b5513c8e08b14b40
hileix <304192604@qq.com>
2019-09-19 13:09:20 +0800

    ✨ (Message) Message 组件使用 scss

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -1,10 +1,7 @@
-import * as React from 'react'
+import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Component } from 'react'
-import * as noop from 'lodash/noop'
-import classNames from 'classnames'
-import { StyledMessage } from './styled'
-import MessageContent from './MessageContent'
+import * as noop from 'lodash/noop';
+import MessageContent from './MessageContent';
 
 export interface MessageTypeProps {
   /** 类名 */
@@ -20,35 +17,34 @@ export type MessageType = 'success' | 'warn' | 'error';
  */
 class Message {
   static success(text: string, duration?: number, callback?: () => void) {
-    return Message.show('success', text, duration, callback)
+    return Message.show('success', text, duration, callback);
   }
 
   static warn(text: string, duration?: number, callback?: () => void) {
-    return Message.show('warn', text, duration, callback)
+    return Message.show('warn', text, duration, callback);
   }
 
   static error(text: string, duration?: number, callback?: () => void) {
-    return Message.show('error', text, duration, callback)
+    return Message.show('error', text, duration, callback);
   }
 
-  static show(type?: MessageType, text?: string, duration = 3000, callback = noop) {
-    const container = document.createElement('div')
-    ReactDOM.render(
-      <MessageContent
-        text={text}
-        type={type}
-      />,
-      container
-    )
+  static show(
+    type?: MessageType,
+    text?: string,
+    duration = 3000,
+    callback = noop
+  ) {
+    const container = document.createElement('div');
+    ReactDOM.render(<MessageContent text={text} type={type} />, container);
 
     setTimeout(() => {
-      const unmountResult = ReactDOM.unmountComponentAtNode(container)
+      const unmountResult = ReactDOM.unmountComponentAtNode(container);
       if (unmountResult !== true) {
-        container.parentNode.removeChild(container)
+        container.parentNode.removeChild(container);
       }
-      callback()
-    }, duration)
+      callback();
+    }, duration);
   }
 }
 
-export default Message
+export default Message;
--- src/components/Message/MessageContent.tsx
+++ src/components/Message/MessageContent.tsx
@@ -1,12 +1,10 @@
 import * as React from 'react';
 import classNames from 'classnames';
 import { Component } from 'react';
-import { StyledMessageContent } from './styled';
-import Portal from '../Portal'
+import Portal from '../Portal';
 import Icon from '../Icon';
 
 export type MessageType = 'success' | 'warn' | 'error';
-
 
 export interface MessageContentProps {
   text?: string;
@@ -17,27 +15,30 @@ const IconTypeMap = {
   success: Icon.Type.success,
   warn: Icon.Type.warning,
   error: Icon.Type.error
-}
+};
 
 class MessageContent extends Component<MessageContentProps, any> {
   render() {
     const { text, type } = this.props;
-    const prefix = 'hmly';
-    const classes = classNames(`${prefix}-message`, {
-      [`${prefix}-message--success`]: type === 'success',
-      [`${prefix}-message--warn`]: type === 'warn',
-      [`${prefix}-message--error`]: type === 'error'
+    const prefix = 'hmly-message';
+    const classes = classNames(prefix, {
+      [`${prefix}--success`]: type === 'success',
+      [`${prefix}--warn`]: type === 'warn',
+      [`${prefix}--error`]: type === 'error'
     });
 
     return (
       <Portal visible>
-        <StyledMessageContent className={classes}>
-          <Icon type={IconTypeMap[type]} className={`${prefix}-message__icon`} />
+        <div className={classes}>
+          <Icon
+            type={IconTypeMap[type]}
+            className={`${prefix}-message__icon`}
+          />
           <span className={`${prefix}-message__text`}>{text}</span>
-        </StyledMessageContent>
+        </div>
       </Portal>
-    )
+    );
   }
 }
 
-export default MessageContent
+export default MessageContent;
