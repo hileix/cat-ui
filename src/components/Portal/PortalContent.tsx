c6bf61faa52b865cb150806aeaf8004f050a13dd
hileix <304192604@qq.com>
2019-09-19 15:46:00 +0800

    ✨ (Portal) Portal 组件使用 scss

--- src/components/Portal/PortalContent.tsx
+++ src/components/Portal/PortalContent.tsx
@@ -1,6 +1,6 @@
-import * as React from 'react'
-import { Component } from 'react'
-import { StyledPortalContent } from './styled'
+import * as React from 'react';
+import { Component } from 'react';
+import classNames from 'classnames';
 
 export interface ProtalProps {
   /** 弹层是否可见 */
@@ -14,26 +14,32 @@ export interface ProtalProps {
 class PortalContent extends Component<ProtalProps, any> {
   static defaultProps = {
     selector: 'body'
-  }
+  };
 
   componentDidMount() {
-    const { onMount } = this.props
-    onMount && onMount()
+    const { onMount } = this.props;
+    onMount && onMount();
   }
 
   componentWillUnmount() {
-    const { onUnmount } = this.props
-    onUnmount && onUnmount()
+    const { onUnmount } = this.props;
+    onUnmount && onUnmount();
   }
 
   render() {
-    const { visible } = this.props
-    // console.log('PortalContent:visible', visible)
+    const { visible } = this.props;
+    const prefix = 'hmly-portal__content';
 
-    return (<StyledPortalContent visible={visible}>
-      {this.props.children}
-    </StyledPortalContent>)
+    return (
+      <div
+        className={classNames(prefix, {
+          [`${prefix}--hide`]: !visible
+        })}
+      >
+        {this.props.children}
+      </div>
+    );
   }
 }
 
-export default PortalContent
+export default PortalContent;
---
+++ src/components/Portal/stories/Portal.stories.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { storiesOf, addDecorator } from '@storybook/react';
+import { actions } from '@storybook/addon-actions';
+import Portal from '../index';
+import '../../../styles/portal.scss';
+import './style.scss';
+import { withInfo } from '@storybook/addon-info';
+import Button from '../../Button';
+
+
+addDecorator(withInfo);
+
+const Demo1 = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <div className='portal-box'>
+      <Button onClick={() => setIsOpen(true)}>打开Portal</Button>
+      <Button onClick={() => setIsOpen(false)}>关闭Portal</Button>
+      <Portal
+        visible={isOpen}
+        onClose={() => setIsOpen(false)}>
+        <div className='portal'>
+          <p>对话框内容...</p>
+          <p>对话框内容...</p>
+          <p>对话框内容...</p>
+        </div>
+      </Portal>
+    </div>
+  )
+};
+
+storiesOf('Protal 传送门', module)
+  .add('Protal', () => <Demo1 />)
