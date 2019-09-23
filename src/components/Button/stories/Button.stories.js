a7e2f7a084388e00bccd8fb28a68066d26aacbec
hileix <304192604@qq.com>
2019-09-23 09:54:30 +0800

    📝 (Button) Update Button stories

--- src/components/Button/stories/Button.stories.js
+++ src/components/Button/stories/Button.stories.js
@@ -1,21 +1,54 @@
-import React from 'react'
-import { storiesOf, addDecorator } from '@storybook/react'
-import { actions } from '@storybook/addon-actions'
-import Button from '../Button'
-import '../styled/Button.scss'
-import { withInfo } from '@storybook/addon-info'
+import React from 'react';
+import { storiesOf, addDecorator } from '@storybook/react';
+import { actions } from '@storybook/addon-actions';
+import Button from '../Button';
+import '../styled/Button.scss';
+import { withInfo } from '@storybook/addon-info';
+import './style.scss';
 
-addDecorator(withInfo)
+addDecorator(withInfo);
+
+const Demo1 = () => {
+  const handleClick = (e) => {
+    console.log('Example:handleClick', e);
+  }
+
+  return (
+    <div className='button-box'>
+      <h3>size</h3>
+      <Button size='sm'>sm</Button>
+      <Button size='md'>md</Button>
+      <Button size='lg'>lg</Button>
+      <Button>default</Button>
+
+      <h3>theme</h3>
+      <Button theme='primary'>primary</Button>
+      <Button theme='primary-up'>primary-up</Button>
+      <Button theme='yellow'>yellow</Button>
+      <Button theme='white-primary'>white-primary</Button>
+      <Button theme='white-cyan'>white-cyan</Button>
+      <Button theme='borderless'>borderless</Button>
+
+      <h3>block</h3>
+      <div className='block-box'>
+        <Button block>block</Button>
+      </div>
+      <div className='block-box'>
+        <Button block theme='yellow'>block</Button>
+      </div>
+      <div className='block-box'>
+        <Button block theme='white-primary' onClick={handleClick}>block</Button>
+      </div>
+
+      <h3>disabled</h3>
+      <Button disabled onClick={handleClick}>disabled</Button>
+      <Button disabled theme='yellow'>disabled</Button>
+      <Button disabled theme='white-primary'>disabled</Button>
+    </div>
+  )
+}
+
 
 storiesOf('Button 按钮', module)
-  .add('sm', () => <Button size="sm">sm</Button>)
-  .add('md', () => <Button size="md">md</Button>)
-  .add('lg', () => <Button size="lg">lg</Button>)
-  .add('primary', () => <Button theme='primary'>primary</Button>)
-  .add('primary-up', () => <Button theme='primary-up'>primary-up</Button>)
-  .add('yellow', () => <Button theme='yellow'>yellow</Button>)
-  .add('white-primary', () => <Button theme='white-primary'>white-primary</Button>)
-  .add('white-cyan', () => <Button theme='white-cyan'>white-cyan</Button>)
-  .add('borderless', () => <Button theme='borderless'>borderless</Button>)
-  .add('block', () => <Button style={{ width: 300 }} block>block</Button>)
-  .add('disabled', () => <Button disabled>disabled</Button>)
+  .add('Button', () => <Demo1 />)
+
