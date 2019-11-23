5816f9f7b9cdab4179d2058333c3d2526c1a6424
hileix <304192604@qq.com>
2019-11-23 14:43:03 +0800

    ✨ (Loading) Loading 添加 size prop

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -2,22 +2,18 @@ import * as React from 'react';
 import * as PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-export type Props = {
-  textColor?: string;
-  bgColor?: string;
-  borderColor?: string;
-};
-
 export interface LoadingProps {
   /** 是否显示 loading */
   visible?: boolean;
   /** 类名 */
   className?: string;
-  /** 行内样式 */
-  style?: React.CSSProperties;
+  /**
+   * loading 的尺寸（高度）
+   */
+  size?: number;
 }
 
-const Loading = ({ visible, className, ...restProps }: LoadingProps) => {
+const Loading = ({ visible, className, size, ...restProps }: LoadingProps) => {
   const prefix = 'cat-loading';
   const classes = classNames(
     prefix,
@@ -26,23 +22,31 @@ const Loading = ({ visible, className, ...restProps }: LoadingProps) => {
     },
     className
   );
+  let lineStyle = {
+    height: size
+  };
+
   return (
     <div className={classes} {...restProps}>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+      <div style={lineStyle} />
+      <div style={lineStyle} />
+      <div style={lineStyle} />
+      <div style={lineStyle} />
+      <div style={lineStyle} />
     </div>
   );
 };
 
 Loading.propTypes = {
-  visible: PropTypes.bool
+  visible: PropTypes.bool,
+  className: PropTypes.string,
+  style: PropTypes.object,
+  size: PropTypes.number
 };
 
 Loading.defaultProps = {
-  visible: false
+  visible: false,
+  size: 24
 };
 
 export default Loading;
--- src/components/Loading/stories/Loading.stories.tsx
+++ src/components/Loading/stories/Loading.stories.tsx
@@ -5,6 +5,7 @@ import '../../../styles/index.scss';
 import './style.scss';
 import Switch from '../../Switch';
 import markdown from '../README.md';
+import DemoWrapper from '@utils/DemoWrapper';
 
 const { useState } = React;
 
@@ -13,12 +14,28 @@ const Demo = () => {
 
   return (
     <div className='loading-wrapper'>
-      <div className='loading-box'>
-        <Loading visible={visible1} />
-      </div>
-      <Switch checked={visible1} onChange={checked => setVisible1(checked)}>
-        显示 Loading
-      </Switch>
+      <DemoWrapper title='基本用法' desc='一个简单的 loading'>
+        <Loading visible />
+      </DemoWrapper>
+
+      <DemoWrapper title='自定义 loading 尺寸' desc='尺寸即高度，默认 24px'>
+        <div className='loading-wrapper__size-wrapper'>
+          <Loading visible size={12} />
+          <span className='loading-wrapper__size-desc'>12px</span>
+        </div>
+        <div className='loading-wrapper__size-wrapper'>
+          <Loading visible size={18} />
+          <span className='loading-wrapper__size-desc'>18px</span>
+        </div>
+        <div className='loading-wrapper__size-wrapper'>
+          <Loading visible size={24} />
+          <span className='loading-wrapper__size-desc'>24px</span>
+        </div>
+        <div className='loading-wrapper__size-wrapper'>
+          <Loading visible size={30} />
+          <span className='loading-wrapper__size-desc'>30px</span>
+        </div>
+      </DemoWrapper>
     </div>
   );
 };
