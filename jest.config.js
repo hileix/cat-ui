058b09e8d635834d009c7d55a138c8b1cecc91c1
hileix <304192604@qq.com>
2019-11-28 14:53:49 +0800

    🔧 (jest) 添加 jest 配置文件

---
+++ jest.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const fs = require('fs-extra');
+
+/**
+ * v2 版本存在的组件
+ * 只有存在的组件才需要测试
+ */
+const existComponents = [
+  'Avatar',
+  'Button',
+  'Drawer',
+  'Guidance',
+  'Icon',
+  'Loading',
+  'Modal',
+  'Question',
+  'Tabs',
+  'notification'
+];
+
+function getTestMatch() {
+  const testMatch = [];
+  const componentsPath = path.resolve(__dirname, 'src/components');
+  const components = fs.readdirSync(componentsPath);
+  components.forEach(name => {
+    if (existComponents.includes(name)) {
+      testMatch.push(`**/${name}/__test__/**/*.[jt]s?(x)`);
+    }
+  });
+  return testMatch;
+}
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  collectCoverage: true,
+  snapshotSerializers: ['enzyme-to-json/serializer'],
+  testMatch: getTestMatch()
+};
