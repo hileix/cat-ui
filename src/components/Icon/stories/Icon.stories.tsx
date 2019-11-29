9821320b8bde8c8bdd735c2a48ca8b54935a6846
hileix <304192604@qq.com>
2019-11-29 16:54:34 +0800

    🐛 (Icon stories) 修复因 message api 修改的问题，导致 Icon stories 引用之前的 Message 而报错的 bug

--- src/components/Icon/stories/Icon.stories.tsx
+++ src/components/Icon/stories/Icon.stories.tsx
@@ -7,7 +7,7 @@ import * as iconTypes from './iconTypes.json';
 import * as ClipboardJS from 'clipboard';
 import markdown from '../README.md';
 
-import Message from '../../Message';
+import message from '../../message';
 
 const { useEffect } = React;
 
@@ -27,7 +27,7 @@ const Demo = () => {
   useEffect(() => {
     const clipboard = new ClipboardJS('.single-icon');
     clipboard.on('success', function(e: any) {
-      Message.success('Coped!');
+      message.success({ content: 'Coped!' });
     });
   }, []);
 
