d197f8b54267011a1ce4d5fd95017571b21416c5
sialvsic <sialvsic@outlook.com>
2019-12-09 16:14:00 +0800

    add Input test

--- src/components/Input/__test__/index.test.tsx
+++ src/components/Input/__test__/index.test.tsx
@@ -1,10 +1,6 @@
 import * as React from 'react'
-import * as Enzyme from 'enzyme'
 import { render, mount } from 'enzyme'
-import * as Adapter from 'enzyme-adapter-react-16'
 import Input from '../index'
-
-Enzyme.configure({ adapter: new Adapter() })
 
 describe('Input', () => {
   test('renders correctly', () => {
