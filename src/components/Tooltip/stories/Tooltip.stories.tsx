5fe76083c72ca13524c5574116e86008ea7ec14d
hileix <304192604@qq.com>
2019-12-13 14:41:27 +0800

    📝 (Tooltip) 修复 Tooltip stories 的错误

--- src/components/Tooltip/stories/Tooltip.stories.tsx
+++ src/components/Tooltip/stories/Tooltip.stories.tsx
@@ -43,7 +43,7 @@ const Demo = () => {
       <div className='tooltip-stories__demo'>
         <div className='tooltip-stories__wrapper tooltip-stories__top'>
           <Tooltip mode={mode} content={'topLeft'} position='topLeft'>
-            <span className='tooltip-stories__button'>span</span>
+            <Button className='tooltip-stories__button'>topLeft</Button>
           </Tooltip>
           <Tooltip mode={mode} content={'top'} position='top'>
             <Button className='tooltip-stories__button'>top</Button>
