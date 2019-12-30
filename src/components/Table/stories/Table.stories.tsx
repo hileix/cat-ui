d44c4d4688702c89e28d24252897cfd04fc482dc
hileix <304192604@qq.com>
2019-12-30 10:55:49 +0800

    ♻️ (Table) 使 Table 组件适配 typescript strict 模式

--- src/components/Table/stories/Table.stories.tsx
+++ src/components/Table/stories/Table.stories.tsx
@@ -19,7 +19,7 @@ const Demo1 = () => {
     return {}
   }
 
-  const onPageChang3 = (page: number) => {
+  const onPageChang3 = (page?: number) => {
     console.log('Example:onPageChang3', page)
     return {}
   }
@@ -32,11 +32,11 @@ const Demo1 = () => {
   const columns1 = [{
     id: 'ranking',
     title: <span>ranking1111</span>,
-    render: text => <a style={{ color: 'red' }} href="javascript:;">{text}</a>
+    render: (text: any) => <a style={{ color: 'red' }} href="javascript:;">{text}</a>
   }, {
     id: 'platforms',
     title: <span>rplatforms222</span>,
-    render: (text, record) => {
+    render: (text: any, record: any) => {
       return (<a style={{ color: 'blue' }} href="javascript:;">
         {text}---{record.rates}
       </a>)
