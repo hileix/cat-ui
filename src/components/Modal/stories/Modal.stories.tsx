5ea8b5b63e8d9d055ba1764459107d16d08fa64c
hileix <304192604@qq.com>
2020-01-08 16:49:33 +0800

    📝 (Modal) 更新 Modal stories

--- src/components/Modal/stories/Modal.stories.tsx
+++ src/components/Modal/stories/Modal.stories.tsx
@@ -10,70 +10,90 @@ import markdown from '../README.md';
 const { useState } = React;
 
 const Demo1 = () => {
-  const [visible1, setvisible1] = useState(false);
-  const [visible2, setvisible2] = useState(false);
   const [visible3, setvisible3] = useState(false);
-  const [visible4, setvisible4] = useState(false);
+
+
+
+  return (
+    <div className='modal-box'>
+      <Button className='modal-button' onClick={() => setvisible3(true)}>
+        关闭 Modal 后自动销毁 children 的 Modal（请打开控制台，查看 DOM）
+      </Button>
+
+
+
+
+
+    </div>
+  );
+};
+
+
+const BaseModal = () => {
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <>
+      <Button className='modal-button' onClick={() => setVisible(true)}>
+        基础 Modal
+      </Button>
+      <Modal
+        title='Enter the amount you want to redeem'
+        visible={visible}
+        onClose={() => setVisible(false)}
+      >
+        <p>
+          Please ensure that the amount you are redeeming does not exceed the
+          remaining total amount.
+        </p>
+      </Modal>
+    </>
+  )
+}
+
+const FooterModal = () => {
+  const [visible, setVisible] = useState(false);
 
   const footer = (
     <div className='footer' style={{ textAlign: 'center' }}>
-      <Button type='primary' onClick={() => setvisible2(false)}>
+      <Button type='primary' onClick={() => setVisible(false)}>
         Submit
       </Button>
     </div>
   );
 
   return (
-    <div className='modal-box'>
-      <Button className='modal-button' onClick={() => setvisible1(true)}>
-        基础 Modal
+    <>
+      <Button className='modal-button' onClick={() => setVisible(true)}>
+        自定义 footer 的 Modal
       </Button>
-      <Button className='modal-button' onClick={() => setvisible2(true)}>
-        自定义 footer 的Modal
-      </Button>
-      <Button className='modal-button' onClick={() => setvisible3(true)}>
-        关闭 Modal 后自动销毁 children 的 Modal（请打开控制台，查看 DOM）
-      </Button>
-      <Button className='modal-button' onClick={() => setvisible4(true)}>
+      <Modal
+        title='Enter the amount you want to redeem'
+        visible={visible}
+        footer={footer}
+        onClose={() => setVisible(false)}
+      >
+        <p>
+          Please ensure that the amount you are redeeming does not exceed the
+          remaining total amount.
+        </p>
+      </Modal>
+    </>
+  )
+}
+
+const NoMaskModal = () => {
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <>
+      <Button className='modal-button' onClick={() => setVisible(true)}>
         没有遮罩
       </Button>
-
       <Modal
         title='Enter the amount you want to redeem'
-        visible={visible1}
-        onClose={() => setvisible1(false)}
-      >
-        <p>
-          Please ensure that the amount you are redeeming does not exceed the
-          remaining total amount.
-        </p>
-      </Modal>
-      <Modal
-        title='Enter the amount you want to redeem'
-        visible={visible2}
-        footer={footer}
-        onClose={() => setvisible2(false)}
-      >
-        <p>
-          Please ensure that the amount you are redeeming does not exceed the
-          remaining total amount.
-        </p>
-      </Modal>
-      <Modal
-        title='Enter the amount you want to redeem'
-        visible={visible3}
-        onClose={() => setvisible3(false)}
-        destroyOnClose
-      >
-        <p>
-          Please ensure that the amount you are redeeming does not exceed the
-          remaining total amount.
-        </p>
-      </Modal>
-      <Modal
-        title='Enter the amount you want to redeem'
-        visible={visible4}
-        onClose={() => setvisible4(false)}
+        visible={visible}
+        onClose={() => setVisible(false)}
         mask={false}
       >
         <p>
@@ -81,12 +101,51 @@ const Demo1 = () => {
           remaining total amount.
         </p>
       </Modal>
-    </div>
-  );
-};
+    </>
+  )
+}
 
-storiesOf('Modal 模态窗', module).add('Modal', () => <Demo1 />, {
-  info: {
-    text: markdown
-  }
-});
+const DestroyOnCloseModal = () => {
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <>
+      <Button className='modal-button' onClick={() => setVisible(true)}>
+        关闭 Modal 后自动销毁 children 的 Modal（请打开控制台，查看 DOM）
+      </Button>
+      <Modal
+        title='Enter the amount you want to redeem'
+        visible={visible}
+        onClose={() => setVisible(false)}
+        destroyOnClose
+      >
+        <p>
+          Please ensure that the amount you are redeeming does not exceed the
+          remaining total amount.
+        </p>
+      </Modal>
+    </>
+  )
+}
+
+storiesOf('Modal 模态窗', module)
+  .add('基础 Modal', () => <BaseModal />, {
+    info: {
+      text: markdown
+    }
+  })
+  .add('自定义 footer', () => <FooterModal />, {
+    info: {
+      text: markdown
+    }
+  })
+  .add('无遮罩', () => <NoMaskModal />, {
+    info: {
+      text: markdown
+    }
+  })
+  .add('关闭 Modal 后自动销毁 children', () => <DestroyOnCloseModal />, {
+    info: {
+      text: markdown
+    }
+  })
